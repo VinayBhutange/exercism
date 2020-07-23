29538ce84f64ee1f1b1590bb793184d81d0243ea
VinayBhutange <kvina573@gmail.com>
2020-07-23 11:51:13 +0530

    Solved

--- cpp/nth-prime/nth_prime.cpp
+++ cpp/nth-prime/nth_prime.cpp
@@ -1,5 +1,31 @@
 #include "nth_prime.h"
+#include <algorithm>
+#include <stdexcept>
+#include <vector>
 
 namespace nth_prime {
+
+std::uintmax_t nth(unsigned int i) {
+  if( i == 0 ) {
+    throw std::domain_error("bad argument");
+  }
+
+  // we can store each prime successively found
+  // in a vector and the next prime is the first
+  // that is not multiple of previous one
+  std::vector<std::uintmax_t> primes;
+  primes.reserve(i);
+
+  for(std::uintmax_t next = 2; primes.size() < i; next++) {
+    if( std::none_of(primes.begin(), primes.end(), [&next](auto previous_prime){
+        return (next % previous_prime) == 0;
+      })
+    ) {
+      primes.push_back(next);
+    }
+  }
+
+  return primes.back();
+}
 
 }  // namespace nth_prime
--- cpp/nth-prime/nth_prime.h
+++ cpp/nth-prime/nth_prime.h
@@ -1,7 +1,11 @@
 #if !defined(NTH_PRIME_H)
 #define NTH_PRIME_H
 
+#include <cstdint>
+
 namespace nth_prime {
+
+std::uintmax_t nth(unsigned int i);
 
 }  // namespace nth_prime
 
